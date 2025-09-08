f1f730818621a0d654f3ddd98e267b44958ba738
Sergey <136964225+deloneint@users.noreply.github.com>
2025-09-09 01:30:56 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const SUPABASE_CONFIG = {
+    url: 'https://wcbzajpsaroldxryvaun.supabase.co', // Замените на ваш URL
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjYnphanBzYXJvbGR4cnl2YXVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNTg0OTUsImV4cCI6MjA3MjkzNDQ5NX0.CijDN8VF-6cjtz4oPwEtimVtIwKFlUtme5bUDEzbW1U' // Замените на ваш ключ
+};
+
+// Экспорт для использования в других файлах
+window.SUPABASE_CONFIG = SUPABASE_CONFIG;
